36cc2e979aee18dc2ba9636e63495cd9de9a95f4
Lukasz Knasiecki <lukasz@huncwot.com>
2026-08-16 19:40:57 +0200

    feat: add /config/.initialized file

--- application/controllers/controller_app_create.php
+++ application/controllers/controller_app_create.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("controller_app.php");           
+require_once("controller_app.php");
 
 /**
  * Controller class for creating new Admin
@@ -17,12 +17,15 @@ class controller_app_create extends controller_app
     public function getContentData()
     {
         if ($this->model->clients->adminExists()) $this->route->redirect('login');
-        $data['view']='create';
-        $data['content']=$this->model->getContentData($this->post);
-        if ($data['content']['created']) $this->route->redirect('login');
-        return $data;        
+        $initialized = $_SERVER['DOCUMENT_ROOT'] . $this->cfg['config_path'] . '/.initialized';
+        if (file_exists($initialized))
+            exit('Error. CMS initialized with empty table.');
+        $data['view'] = 'create';
+        $data['content'] = $this->model->getContentData($this->post);
+        if ($data['content']['created']) {
+            file_put_contents($initialized, date('Y-m-d H:i:s'));
+            $this->route->redirect('login');
+        }
+        return $data;
     }
-
 }
-
-?>
